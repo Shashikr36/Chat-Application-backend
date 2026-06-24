d933f00f9e7fb213436f4a779ce788c7c59767da
Shashikr36 <shashikrpatwa36@gmail.com>
2026-06-24 23:51:01 +0530

    update env variable

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,13 +1,20 @@
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 
+const allowedOrigins = process.env.FRONTEND_URL
+    ? process.env.FRONTEND_URL.split(",")
+    : ["http://localhost:5173"];
+
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "https://chat-application-frontend-theta.vercel.app"],
+        origin: allowedOrigins,
     },
 });
 
